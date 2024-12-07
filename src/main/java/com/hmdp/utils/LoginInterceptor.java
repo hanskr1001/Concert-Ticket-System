c9f597ae0d0d29c4cdbd836921c25df6f89529ab
Reol <liujimao1001@gmail.com>
2024-12-07 21:33:33 +0800

    启用登录拦截

--- src/main/java/com/hmdp/utils/LoginInterceptor.java
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -8,10 +8,10 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        /*if(UserHolder.getUser() == null){
+        if(UserHolder.getUser() == null){
             response.setStatus(401);
             return false;
-        }*/
+        }
         return true;
     }
 }
